4521e41e71ae9ccd5408e0e2f7e3bfdc55d48311
bear <thedarrendawson@gmail.com>
2020-04-24 21:28:45 -0700

    First commit

---
+++ src/Components/CommunityFundPage/CommunityFundPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './CommunityFundPage.css';
+
+class CommunityFundPage extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div id="CommunityFundPage">
+        
+      </div>
+    );
+  }
+
+}
+
+export default CommunityFundPage;
---
+++ src/Components/Template.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './[name].css';
+
+class [name] extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div id="[name]">
+
+      </div>
+    );
+  }
+
+}
+
+export default [name];
