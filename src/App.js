89a2ef6ca5e21a87a4b339db32a325769a6f1ddd
skydereign <skydereign@gmail.com>
2020-04-24 23:55:29 -0700

    Move firebase logic to firebase test components

--- src/App.js
+++ src/App.js
@@ -1,62 +1,21 @@
 import React from 'react';
 import './App.css';
-import firebase from './firebase.js'
 
 import CommunityFundPage from './Components/CommunityFundPage/CommunityFundPage.js';
+import FirebaseApp from './Components/FirebaseTest/FirebaseApp.js';
 
 class App extends React.Component {
 
   constructor() {
     super();
-    this.state = {
-      list: [{id: 1}, {id: 2}, {id: 3}]
-    };
-    this.testSubmit = this.testSubmit.bind(this);
-  }
-
-  componentDidMount() {
-    const itemsRef = firebase.database().ref('items');
-    itemsRef.on('value', (snapshot) => {
-      let items = snapshot.val();
-      let newState = [];
-      for (let item in items) {
-        newState.push({
-          id: item,
-          title: items[item].title,
-          user: items[item].user
-        });
-      }
-      this.setState({
-        items: newState
-      });
-    });
-  }
-
-  testSubmit(e) {
-    e.preventDefault();
-    const ownersRef = firebase.database().ref('owners');
-    const owner = {
-      owner: "ciceros",
-      img: "invalid image"
-    }
-    ownersRef.push(owner);
-    console.log("this is a test");
+    this.state = {};
   }
 
   render() {
     return (
       <div id="App">
         <div id="website_column">
-          <button onClick={this.testSubmit}>this is a test</button>
-          <ul>
-            { this.state.list.map((item) => {
-              return (
-                <li key={item.id}>
-                  entry {item.id}
-                </li>
-              )
-            })}
-          </ul>
+          <FirebaseApp />
         </div>
       </div>
     );
