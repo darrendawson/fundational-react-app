55a77491976cc0408a99b02093b750bda13f140c
skydereign <skydereign@gmail.com>
2020-04-24 22:46:52 -0700

    Add test write to firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase.js'
 
 import CommunityFundPage from './Components/CommunityFundPage/CommunityFundPage.js';
 
@@ -8,14 +8,55 @@ class App extends React.Component {
 
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      list: [{id: 1}, {id: 2}, {id: 3}]
+    };
+    this.testSubmit = this.testSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    const itemsRef = firebase.database().ref('items');
+    itemsRef.on('value', (snapshot) => {
+      let items = snapshot.val();
+      let newState = [];
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title: items[item].title,
+          user: items[item].user
+        });
+      }
+      this.setState({
+        items: newState
+      });
+    });
+  }
+
+  testSubmit(e) {
+    e.preventDefault();
+    const ownersRef = firebase.database().ref('owners');
+    const owner = {
+      owner: "ciceros",
+      img: "invalid image"
+    }
+    ownersRef.push(owner);
+    console.log("this is a test");
   }
 
   render() {
     return (
       <div id="App">
         <div id="website_column">
-          <CommunityFundPage/>
+          <button onClick={this.testSubmit}>this is a test</button>
+          <ul>
+            { this.state.list.map((item) => {
+              return (
+                <li key={item.id}>
+                  entry {item.id}
+                </li>
+              )
+            })}
+          </ul>
         </div>
       </div>
     );
