ab13b25edf4c26930c9a22968a716118507080f4
bear <thedarrendawson@gmail.com>
2020-04-25 19:44:00 -0700

    Add heart icon to Navbar logo

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,11 +7,11 @@ class Navbar extends React.Component {
     super();
     this.state = {};
   }
-  
+
   render() {
     return (
       <div id="Navbar">
-        <h1 className="navbar_title">Fundational</h1>
+        <h1 className="navbar_title">F<span id="heart_icon" className="color_txt_primary">&hearts;</span>ndational</h1>
         <div className="links_container">
           <h3 className="navbar_title link_spacing">Link1</h3>
           <h3 className="navbar_title link_spacing">Link2</h3>
